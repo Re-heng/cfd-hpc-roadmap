5086476ab4dab4b64db90149ba078e7623079c97
Re-heng <hengbao2003@stu.xjtu.edu.cn>
2026-04-28 20:23:32 +0800

    add output csv

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 
 int index(int x, int y, int nx)
 {
     return nx * y + x;
+}
+
+void write_csv(const std::vector<double> &u,
+               const std::string &filename,
+               int nx,
+               int ny)
+{
+    std::ofstream file(filename);
+    for (int j = 0; j < ny; j++)
+    {
+        for (int i = 0; i < nx; i++)
+        {
+            if (i == nx - 1)
+            {
+                file << u[index(i, j, nx)];
+            }
+            else
+            {
+                file << u[index(i, j, nx)];
+                file << ',';
+            }
+        }
+        if (j != ny - 1)
+        {
+            file << '\n';
+        }
+    }
 }
 
 int main()
@@ -28,5 +57,8 @@ int main()
         }
         std::cout << std::endl;
     }
+
+    write_csv(u, "results/grid_demo.csv", nx, ny);
+    std::cout << "Wrote results/grid_demo.csv\n";
     return 0;
 }
